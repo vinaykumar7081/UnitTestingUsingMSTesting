4f2680e13e3ae06a88cbea9304837e6f0a1f57b3
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-13 12:50:10 +0530

    [VINAY] Add: Checking the Customer coffee is received or not using Mock Testing

--- CoffeeMakingTesting/UnitTest1.cs
+++ CoffeeMakingTesting/UnitTest1.cs
@@ -1,5 +1,6 @@
 using CoffeeMaker;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace CoffeeMakingTesting
 {
@@ -12,6 +13,16 @@ namespace CoffeeMakingTesting
             StarBuksStore store = new StarBuksStore(new FakeStarBuks());
             string actual = store.OrderCoffee(5, 10);
             Assert.AreEqual(actual, "Your Coffee is Recieve");
+        }
+        [TestMethod]
+        public void OrderCoffeeMoq_InStarBucks_ShouldReturnRecieveMessage()
+        {
+            var moqService = new Mock<InterfaceMakeACoffee>();
+            moqService.Setup(y => y.CheckingIngredientsAvailability()).Returns(true);
+            moqService.Setup(y => y.CoffeeMaking(5, 10)).Returns("Your Coffee is Recieve");
+            StarBuksStore result = new StarBuksStore(moqService.Object);
+            string actual = result.OrderCoffee(5, 10);
+                Assert.AreEqual(actual, "Your Coffee is Recieve");
         }
     }
 }
