8f36e0873a6b290fcb119bc1f5db86100bf0ce28
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-16 13:28:39 +0530

    [VINAY] Add: Using Reflection displaying the properties and Methods  of the class

---
+++ Reflection/Customer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    /// <summary>
+    /// Customer is An class wheather declare a methods property and Constructors
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// Gets or Sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Gets or Sets the Name.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        ///<summery>
+        /// <see cref="Reflection.Customer"/>
+        /// </summery>
+        /// <param name="id">The identifie</param>>
+        /// <param name="name">The identifie</param>>
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        ///<summary>
+        ///prints the identifier.
+        ///</summary>
+        public void PrintId()
+        { 
+        Console.WriteLine("Id is "+this.Id);
+        }
+        ///<summary>
+        ///prints the name.
+        ///</summary>
+        public void PrintName()
+        {
+            Console.WriteLine("Name is :->" + this.Name);
+        }
+        public static void Test()
+        { 
+        Type type = Type.GetType("Reflection.Customer");
+            Console.WriteLine("FullName is :" +type.FullName);
+            Console.WriteLine("class Name is :" + type.Name);
+            Console.WriteLine("Methods in Customer Class");
+            ////storing the number of methods in an Array which ifs of method info class.
+            MethodInfo[] methods = type.GetMethods();
+            foreach (MethodInfo method in methods)
+            { 
+            Console.WriteLine(method.ReturnType.Name+" "+method.Name);
+            }
+            Console.WriteLine("properties in customer class");
+            PropertyInfo[] properties = type.GetProperties();
+            foreach (PropertyInfo propertie in properties)
+            {
+                Console.WriteLine(propertie.PropertyType.Name + " " + propertie.Name);
+            }
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            { 
+            Console.WriteLine(constructor.ToString());
+            }
+
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Reflection;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Customer customer = new Customer(105,"vinay");
+        customer.PrintName();
+        customer.PrintId();
+        Customer.Test();
+    }
+}
